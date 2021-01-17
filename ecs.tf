d5df666f0f22add4bf5aed1f636cbb38bb5da724
tom-256 <t11a.256.512.1024@gmail.com>
2021-01-17 18:36:56 +0900

    use ecr

--- ecs.tf
+++ ecs.tf
@@ -24,13 +24,32 @@ resource "aws_security_group_rule" "ecs" {
   cidr_blocks       = ["10.0.0.0/16"]
 }
 
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
 resource "aws_ecs_task_definition" "service_a" {
   family                   = "service"
-  container_definitions    = file("task-definitions/service.json")
+  container_definitions    = <<DEFINITIONS
+  [
+    {
+      "name": "service-a",
+      "image": "${aws_ecr_repository.main.repository_url}",
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ]
+    }
+  ]
+  DEFINITIONS
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
   network_mode             = "awsvpc"
+  depends_on               = [aws_ecr_repository.main]
+  execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
 }
 
 resource "aws_ecs_cluster" "main" {
@@ -52,7 +71,7 @@ resource "aws_ecs_service" "service_a" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.main.arn
-    container_name   = "nginx"
+    container_name   = "service-a"
     container_port   = 80
   }
 }
